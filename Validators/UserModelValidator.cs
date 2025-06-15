c67ddb2c67d0a47acaee466f14b63784e6daced6
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-15 17:05:35 +0400

    changed some lines
    remove injected CreateUserDto from ctor and edit rule for roles

--- Validators/UserModelValidator.cs
+++ Validators/UserModelValidator.cs
@@ -5,9 +5,9 @@ using JobSeekerAPI.Services;
 
 namespace JobSeekerAPI.Validators
 {
-    public class CreateUserDtoValidator:AbstractValidator<CreateUserDto>
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
     {
-        public CreateUserDtoValidator(IUserService userService) 
+        public CreateUserDtoValidator() 
         { 
             RuleFor(user => user.FirstName)
                 .NotEmpty().WithMessage("First name is required.")
@@ -21,13 +21,13 @@ namespace JobSeekerAPI.Validators
                 .NotEmpty().WithMessage("Email is required.")
                 .EmailAddress().WithMessage("Invalid email format.");
 
-            RuleFor(user => user.HashedPassword)
+            RuleFor(user => user.Password)
                 .NotEmpty().WithMessage("Password is required.")
                 .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
 
             RuleFor(user => user.Role)
                 .NotEmpty().WithMessage("Role is required.")
-                .Must(role => role == "hr" || role == "admin" || role == "applicant")
+                .Must(role => role.ToLower() == "hr" || role.ToLower() == "admin" || role.ToLower() == "applicant")
                 .WithMessage("Role must be either 'hr', 'admin', or 'applicant'.");
         }
     }
