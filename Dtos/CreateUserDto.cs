6f03325e5ca6e3ee9460c3f0e7f8eaaae098377f
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-15 16:59:18 +0400

    rename HashedPassword to Password

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -5,7 +5,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string HashedPassword { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty; // hr, admin, applicant
     }
 }
