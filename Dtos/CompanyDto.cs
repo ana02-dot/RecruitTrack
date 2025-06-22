2332a62d81532a914fb2836b043c4db9efd1d9d2
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-22 22:18:54 +0400

    add Dto for company update and create

---
+++ Dtos/CompanyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JobSeekerAPI.Dtos
+{
+    public class CompanyDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Industry { get; set; } = string.Empty;
+    }
+}
