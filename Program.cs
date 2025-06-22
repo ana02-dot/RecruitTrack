e1c128a1f27197fb12e44b9ed0e599ad735244e9
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-22 22:33:03 +0400

    implement validators, controllers, services, middleware

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using JobSeekerAPI.Data;
 using JobSeekerAPI.Dtos;
+using JobSeekerAPI.Middleware;
 using JobSeekerAPI.Repositories;
 using JobSeekerAPI.Services;
 using JobSeekerAPI.Validators;
@@ -9,15 +10,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add logging
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<IApplicantRepository, ApplicantRepository>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<IApplicantService, ApplicantService>();
 
 builder.Services.AddScoped<IValidator<CreateUserDto>, CreateUserDtoValidator>();
+builder.Services.AddScoped<IValidator<CompanyDto>, CompanyModelValidator>();
+builder.Services.AddScoped<IValidator<ApplicantDto>, ApplicantModelValidator>();
 
 builder.Services.AddControllers();
 
@@ -39,7 +50,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Add exception handling middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 app.UseHttpsRedirection();
+
+/*
+app.UseStaticFiles();
+*/
 
 app.UseAuthorization();
 
