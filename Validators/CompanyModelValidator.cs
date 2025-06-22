a69239c5e588897c903632109029894655202c7b
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-22 22:19:43 +0400

    add company model validation
    rules for companyDto fields

---
+++ Validators/CompanyModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using JobSeekerAPI.Dtos;
+
+namespace JobSeekerAPI.Validators
+{
+    public class CompanyModelValidator : AbstractValidator<CompanyDto>
+    {
+        public CompanyModelValidator()
+        {
+            RuleFor(company => company.Name)
+                .NotEmpty().WithMessage("Company name is required.")
+                .Length(2, 100).WithMessage("Company name must be between 2 and 100 characters.");
+            RuleFor(company => company.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .Length(6, 200).WithMessage("Company Description must be between 6 and 200 characters.");
+            RuleFor(company => company.Industry)
+                .NotEmpty().WithMessage("Industry is required.")
+                .Length(2, 50).WithMessage("Company industry must be between 2 and 50 characters.");
+        }
+    }
+}
