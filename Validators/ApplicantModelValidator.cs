edcf094a37706c0f5a7144e9ca8e7eb28a135e9e
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-22 22:25:28 +0400

    add validation for ApplicantDto.cs

---
+++ Validators/ApplicantModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using JobSeekerAPI.Dtos;
+
+namespace JobSeekerAPI.Validators;
+
+public class ApplicantModelValidator:AbstractValidator<ApplicantDto>
+{
+    public ApplicantModelValidator()
+    {
+        RuleFor(applicant => applicant.FirstName)
+            .NotEmpty().WithMessage("First Name  is required.")
+            .Length(2, 50).WithMessage("First name must be between 2 and 50 characters.");
+        RuleFor(applicant => applicant.LastName)
+            .NotEmpty().WithMessage("Last Name  is required.")
+            .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters.");
+        RuleFor(applicant => applicant.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+    }
+}
