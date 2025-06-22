19ba636bd02b06c6f5dde8621d6593204bad9e9a
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-06-22 22:30:48 +0400

    some changes

---
+++ Dtos/ApplicantDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace JobSeekerAPI.Dtos;
+
+public class ApplicantDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public IFormFile? Resume { get; set; }
+}
--- Models/Applicant.cs
+++ Models/Applicant.cs
@@ -5,8 +5,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string HashedPassword { get; set; } = string.Empty;
-        public string Resume { get; set; } = string.Empty; // URL or path to the resume file
+        public string? ResumePath { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
